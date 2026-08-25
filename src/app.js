39bc4cfc86da02d16b4e63d2b6ea5b9713474419
Aziz Alfauzi <azizalfauzi65@gmail.com>
2026-08-25 19:48:44 +0700

    pemanggilan semua fungs pada app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const app = express();
-const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("API Node.js Berjalan!");
-});
+// Middleware parsing JSON dari request body
+app.use(express.json());
 
+// Import Routes
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+// Gunakan Routes dengan Prefix URL
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+// Port aplikasi
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server berjalan di http://localhost:${PORT}`);
 });
