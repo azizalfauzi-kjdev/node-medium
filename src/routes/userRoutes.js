0a2e68046276ebc2255f299f350167d36f8497fd
Aziz Alfauzi <azizalfauzi65@gmail.com>
2026-08-25 20:42:47 +0700

    fixin bug penamaah pada controllers

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/userController");
+const userController = require("../controllers/userControllers");
 const { verifyToken, authorizeRoles } = require("../middleware/authMiddleware");
 
 // Semua rute di bawah ini wajib membawa Token JWT
